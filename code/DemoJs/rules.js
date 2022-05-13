138316b6cfbc9e396bfdc608b05393579f90cc15
Patrick Drechsler <patrick@pdrechsler.de>
2022-05-13 20:48:39 +0200

    ✅ add parameterized test and comment unused code

--- code/DemoJs/rules.js
+++ code/DemoJs/rules.js
@@ -8,40 +8,43 @@ function isValid(s) {
         return false;
     }
 
-    if (s.length > 30) {
+    if (s.length > 10) {
         return false;
     } else {
         return true;
     }
 }
 
-function anotherValidation(a, b) {
-    if (a + b == 10) return true;
-    if (a - b == 10) return true;
-    if (a * b == 10) return true;
-    if (a / b == 10) return true;
-    if (a % b == 1) return true;
-}
+// function anotherValidation(a, b) {
+//     if (a + b == 10) return true;
+//     if (a - b == 10) return true;
+//     if (a * b == 10) return true;
+//     if (a / b == 10) return true;
+//     if (a % b == 1) return true;
+// }
 
-function looping(a)
-{
-    let result = 0;
-    for (const i = 0; i < a; i++)
-    {
-        result++;
-    }
-    return result;
-}
+// function looping(a)
+// {
+//     let result = 0;
+//     for (const i = 0; i < a; i++)
+//     {
+//         result++;
+//     }
+//     return result;
+// }
 
-function logical(b1, b2)
-{
-    if (b1 && b2) return true;
-    if (b1 || b2) return true;
-    return false;
-}
+// function logical(b1, b2)
+// {
+//     if (b1 && b2) return true;
+//     if (b1 || b2) return true;
+//     return false;
+// }
 
-function getFirst(numbers) {
-    return numbers[0];
-}
+// function getFirst(numbers) {
+//     return numbers[0];
+// }
 
-module.exports = { helloWorld, isValid, anotherValidation, looping, logical, getFirst };
+// module.exports = { helloWorld, isValid, anotherValidation, looping, logical, getFirst };
+
+
+module.exports = { helloWorld, isValid };
--- code/DemoJs/rules.test.js
+++ code/DemoJs/rules.test.js
@@ -1,6 +1,21 @@
-const assert = require('assert');
 const {helloWorld, isValid, anotherValidation, looping, logical, getFirst} = require('./rules.js');
 
 test('helloWorld', () => {
    expect(helloWorld()).toBe('Hello World');
+});
+
+
+describe('isValid', () => {
+   
+   it.each([
+      [null, false],
+      [undefined, false],
+      ['', false],
+      ['validinput', true],
+      ['0123456789', true],
+      ['01234567891', false],
+   ])('when given %p returns %p', (input, expected) => {
+      expect(isValid(input)).toEqual(expected);
+   });
+
 });
