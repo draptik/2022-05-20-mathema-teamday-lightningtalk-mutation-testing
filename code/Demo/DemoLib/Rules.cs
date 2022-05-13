ad74695f1201c19eca6b524ead0e5eacfed43034
Patrick Drechsler <patrick@pdrechsler.de>
2022-05-13 15:32:44 +0200

    🎉 add csharp demo projects

---
+++ code/Demo/DemoLib/Rules.cs
@@ -0,0 +1,50 @@
+﻿namespace DemoLib;
+
+public class Rules
+{
+    public bool IsValid(string s)
+    {
+        if (string.IsNullOrWhiteSpace(s))
+        {
+            return false;
+        }
+
+        if (s.Length > 3)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public bool AnotherValidation(int a, int b)
+    {
+        if (a + b == 10) return true;
+        if (a - b == 10) return true;
+        if (a * b == 10) return true;
+        if (a / b == 10) return true;
+        if (a % b == 1) return true;
+        return false;
+    }
+    
+    public int Looping(int a)
+    {
+        var result = 0;
+        for (int i = 0; i < a; i++)
+        {
+            result++;
+        }
+        return result;
+    }
+
+    public bool Logical(bool b1, bool b2)
+    {
+        if (b1 && b2) return true;
+        if (b1 || b2) return true;
+        return false;
+    }
+
+    public int GetFirst(IEnumerable<int> numbers) => numbers.First();
+}
