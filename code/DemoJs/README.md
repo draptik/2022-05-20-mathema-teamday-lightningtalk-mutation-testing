6a602701cc842ede8cb1d2738aad5c66c0daeb01
Patrick Drechsler <patrick@pdrechsler.de>
2022-05-13 17:35:47 +0200

    🚧 trying to configure strykerJs...

---
+++ code/DemoJs/README.md
@@ -0,0 +1,12 @@
+# StrykerJS
+
+Documentation for StrykerJS: https://stryker-mutator.io/docs/stryker-js/introduction/
+
+## Prerequisites
+
+None.
+
+## Usage
+
+- run tests: `./run-tests.sh`
+- run stryker: `./run-stryker.sh` (WIP)
--- code/DemoJs/rules.test.js
+++ code/DemoJs/rules.test.js
@@ -1,5 +1,7 @@
-import assert from 'assert';
-import rules from './rules.js';
+// import assert from 'assert';
+// import rules from './rules.js';
+const assert = require('assert');
+const rules = require('./rules.js');
 
 describe('rules', () => {
    describe('initialize', () => {
