be18349dca78e5f895df792cddde0d2fb56d1efc
Patrick Drechsler <patrick@pdrechsler.de>
2022-05-13 17:42:32 +0200

    🚧 code: js -> using import instead of require -> tests run, but stryker fails

--- code/DemoJs/rules.test.js
+++ code/DemoJs/rules.test.js
@@ -1,7 +1,7 @@
-// import assert from 'assert';
-// import rules from './rules.js';
-const assert = require('assert');
-const rules = require('./rules.js');
+import assert from 'assert';
+import rules from './rules.js';
+// const assert = require('assert');
+// const rules = require('./rules.js');
 
 describe('rules', () => {
    describe('initialize', () => {
